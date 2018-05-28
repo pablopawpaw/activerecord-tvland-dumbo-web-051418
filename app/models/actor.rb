27d9580e92768d6b629ea0f3dbbdf06bdab59da3
pablopawpaw <lpault29@gmail.com>
2018-05-28 19:30:07 -0400

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,13 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+  def list_roles
+    # self.shows.merge(self.characters)
+    self.shows.map do |show|
+      "#{show.characters.first.name} - #{show.name}"
+    end
+  end
 end
---
+++ db/migrate/008_add_foreign_keys_to_characters.rb
@@ -0,0 +1,5 @@
+class AddForeignKeysToCharacters < ActiveRecord::Migration[5.1]
+  def change
+    add_column :characters, :actor_id, :integer
+  end
+end
